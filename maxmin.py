c7589b642b4aecb306c4365590cf40efd119d7ea
Governor <elimushwari@gmail.com>
2017-02-08 09:21:30 +0300

    a file with the find_max_min function

---
+++ maxmin.py
@@ -0,0 +1,10 @@
+
+def find_max_min(list_of_nums):
+  """ This function is meant to return an Array (List) of the maximum and minmum of the inpt array 
+  list_of_nums"""
+  
+  if max(list_of_nums) == min(list_of_nums): #for a list with identical numbers
+    return [max(list_of_nums)]
+  
+  else:
+    return [min(list_of_nums), max(list_of_nums)]
