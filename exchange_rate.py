61de495cece5f12a659dab714c0ed1e707e47a38
Governor <elimushwari@gmail.com>
2017-02-08 16:24:24 +0300

    added the complete file

--- exchange_rate.py
+++ exchange_rate.py
@@ -3,8 +3,11 @@ def find_exchange_rate():
 	"""This is a simple command line application to convert one currency into the other
 	using the fixer.io API"""
 	
+	from urllib2 import Request, urlopen
+	import 
+	
 	curr1 = input("Convert FROM (e.g USD, GBP):")
-	curr1 = curr1upper()
+	curr1 = curr1.upper()
 	
 	curr2 = input("Convert To (e.g GBP, USD)")
 	curr2 = curr2.upper()
@@ -12,10 +15,13 @@ def find_exchange_rate():
 	amount = float(input("Please Enter the AMOUNT to be Converted:"))
 	
 	api_url = ("http://api.fixer.io/latest?symbols=%s,%s") % (curr1, curr2)
-	exchange_rate = requests.get(api_url).json()["rates"][curr2]
+	r = Request(api_url)
+	exchange_rate = json.loads(urlopen(r).read())
+	print(exchange_rate)
 	converted_amount = amount * exchange_rate
 	
-	return curr1 + str(amount) + " converts to " + curr1 + str(converted_amount)
-	
+	return curr1 + str(amount) + " converts to " + curr1 + str(converted_amount)"""
+
+
 	
 	
